3d6a220547a146d8dd1604a63cad392cebf8c4fb
Derrak Richard <derrak@gmail.com>
2022-08-12 10:45:44 -0700

    add Idenity configs

--- FlavorSite/Startup.cs
+++ FlavorSite/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
 using FlavorSite.Models;
 
 namespace FlavorSite
@@ -27,20 +28,25 @@ namespace FlavorSite
       services.AddEntityFrameworkMySql()
         .AddDbContext<FlavorSiteContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<FlavorSiteContext>()
+            .AddDefaultTokenProviders();
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+      app.UseAuthentication();
       app.UseRouting();
-
+      app.UseAuthorization();
       app.UseEndpoints(routes =>
       {
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
 
       app.UseStaticFiles();
-      
+
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World! We've got an error");
