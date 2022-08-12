f8c7245be33d426f7d4e92550cc21b84ac87a1c5
Derrak Richard <derrak@gmail.com>
2022-08-12 13:33:21 -0700

    update accountController w FlavorSiteContext

--- FlavorSite/Controllers/AccountController.cs
+++ FlavorSite/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace FlavorSite.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, FlavorSiteContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
