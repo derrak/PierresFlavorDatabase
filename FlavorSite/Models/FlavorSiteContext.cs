4f28a6ba20c0ff248def604405b6de9e9a5f65da
Derrak Richard <derrak@gmail.com>
2022-08-12 09:21:16 -0700

    add FlavorSiteContext model

---
+++ FlavorSite/Models/FlavorSiteContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace FlavorSite.Models
+{
+  public class FlavorSiteContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public FlavorSiteContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
+    
